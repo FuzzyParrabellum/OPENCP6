62a58c12f40e92f7b2b2b2f5fd0a41cb9aff2a01
Fuzzy Parabellum <antrod.pro@hotmail.fr>
2021-08-21 17:49:16 +0200

    crée maintenant une branche 4test2108 pour le problème de la boucle dans retrieve35movies qui ne peut pas concatener plusieurs array pour l'instant, va donc faire la boucle à la main dans un 1er temps

--- Frontend/interactivity.js
+++ Frontend/interactivity.js
@@ -18,12 +18,9 @@ let divHistoryRightArrow = ''
 let divComedyLeftArrow = ''
 let divComedyRightArrow = ''
 
-console.log(divBestMovieRightArrow);
-console.log("okok");
 
 let BestRA = document.getElementById(divBestMovieRightArrow)
-console.log(BestRA)
-console.log(document.getElementById('BestRatedAR'))
+
 // Le nombre de films à retourner par catégorie
 const NumMoviesToShow = 7
 
@@ -42,7 +39,6 @@ let fetch_movies = function(url, div, numberOfMovies, genre='', pageNumber = 1) 
 
 // Fonction permettant de fetch les 5 premiers films 
 let retrieveFirst5Movies = function (url, div, genre='', pageNumber = 1) {
-    console.log("retieveFIrst5Movies is called");
     divToChange = document.getElementById(div)
         fetch(url + genre, {
             method: "GET",
@@ -74,7 +70,6 @@ let retrieveFirst5Movies = function (url, div, genre='', pageNumber = 1) {
 // Fonction permettant de fetch les deux films après les 5 premiers, étant donné que l'API OC-Movies
 // ne peut renvoyer les informations de seulement 5 films par page.
 let retrieveRestOfMovies = function(div, numberOfMovies, genre='', pageNumber = 1) {
-    console.log("retieveRestOfMovies is called");
     divToChange = document.getElementById(div)  
     movieLeftToRetrieve = numberOfMovies % 5
     fetch('http://localhost:8000/api/v1/titles/?page=' + (pageNumber+1).toString() + '&sort_by=-imdb_score' + genre, {
@@ -105,7 +100,6 @@ let retrieveRestOfMovies = function(div, numberOfMovies, genre='', pageNumber = 
 
 // Fonction permettant de fetch un seul film
 let retrieveFirstMovie = function (url, div, genre='') {
-    console.log("retrieveFirstMovie");
     divToChange = document.getElementById(div)
         fetch(url + genre, {
             method: "GET",
@@ -121,11 +115,7 @@ let retrieveFirstMovie = function (url, div, genre='') {
                 let movie_id = data.results[0].id
                 let movie_title = data.results[0].title
                 titleToChange = document.getElementById(divTitleToChange)
-                console.log(titleToChange)
-                console.log(titleToChange.innerHTML)
                 titleToChange.innerHTML = movie_title
-                console.log(titleToChange)
-                console.log(titleToChange.innerHTML)
                 let img_url = data.results[0].image_url
                 let new_img = document.createElement("img")
                 new_img.setAttribute("id", movie_id)
@@ -155,12 +145,104 @@ document.addEventListener("DOMContentLoaded", function() {
     
 })
 
+// On ajoute des event Listener pour chaque flèche permettant de faire défiler les films
+BestRA.addEventListener("click", async function() {
+    // il faut ici trouver un moyen de savoir sur quel page number on est
+    let new_movie_img = await fetch_movies_onClick('http://localhost:8000/api/v1/titles/', '', 1)
+    console.log('new_movie_img is ' + new_movie_img)
+    // Dans un 1er temps, déplier la liste pour mettre les 35 films côte à côte genre de 0-34
+    all_movie_img = []
+    for (list_of_5_movies in new_movie_img) {
+        for (movie in list_of_5_movies) {
+            all_movie_img.push(movie)
+        }
+    }
+    console.log(all_movie_img)
+    // suivant le page Number, on va prendre un bout de cette liste, qui va constituer les 7 images à afficher
+    // pour chacune des 7 images affichées actuellement, càd chaque div, enfant de la div principale dans laquelle on opère,
+    // on va changer deux attributs à l'image, son url et son id
+    // Donc boucle for qui compare chaque div avec chaque nouvelle image et qui change les propriétés
+
+})
+
+// // Fonction permettant de fetch de nouveaux films après qu'on ai click sur une flèche
+// let fetch_movies_onClick = function(url, genre='', pageNumber = 1) {
+
+//     full_list_of_img = []
+//     for (let counter = 0; counter < 7; counter++) {
+//         list_of_img = setTimeout(() => retrieve35Movies(url, genre, pageNumber), 500);
+//         console.log("the list of img is " + list_of_img)
+//         full_list_of_img.push(list_of_img)
+//         pageNumber++;
+//     }
+//     // console.log(full_list_of_img[0])
+//     // console.log(full_list_of_img[0][0])
+    
+// }
+
+// Il faudrait que cette version de la fonction marche pour que retrieve35Movies soit appellée de la bonne façon
+let fetch_movies_onClick = async function(url, genre='', pageNumber = 1) {
+
+    full_list_of_img = []
+    for (let counter = 0; counter < 7; counter++) {
+        list_of_img = await retrieve35Movies(url, genre, pageNumber);
+        console.log("the list of img is " + list_of_img)
+        full_list_of_img.push(list_of_img)
+        pageNumber++;
+    }
+    return full_list_of_img
+    
+}
+
+
+// Fonction permettant de fetch 5 autres films après un clic sur une flèche sur le coté d'une catégorie
+let retrieve35Movies = async function (url, genre='', pageNumber = 1) {
+    console.log("retieve35Movies is called");
+    var url = url + "?page=" + pageNumber + "&sort_by=-imdb_score" + genre
+    let fetching = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+            }
+        })
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            let movies_img = [];
+            let addImg = function (imgToAdd) {
+                movies_img = [...movies_img, ...imgToAdd]
+            }
+            for (let counter = 0; counter < 5; counter++) {
+                let movie_components = []
+                let movie_id = data.results[counter].id
+                let img_url = data.results[counter].image_url
+                movie_components = [movie_id, img_url]
+                console.log("movie_compenents is " + movie_components)
+                addImg(movie_components);
+                console.log("the counter is " + counter)
+                
+                console.log("movies_img is " + movies_img)
+            }
+            
+            return movies_img
+        })
+        .catch((err) => {
+            console.log("there is an error")
+            console.log(err)
+        })
+        console.log("fetching is" + fetching)
+        console.log("OH OH OH OH OH OH")
+        return fetching
+}
+
 
 // Le Code pour ouvrir une fenêtre modale pour le meilleur film
 var modal = document.getElementById("modalBestMovie");
 
 var btn = document.getElementById("BMPlayButton");
-console.log(btn + " is btn")
+
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
@@ -176,78 +258,4 @@ window.onclick = function(event) {
       modal.style.display = "none";
     }
   }
-
-
-
-
-// Fonction permettant de fetch de nouveaux films après qu'on ai click sur une flèche
-let fetch_movies_onClick = function(url, genre='', pageNumber = 1) {
-
-    full_list_of_img = []
-    for (let counter = 0; counter < 7; counter++) {
-        list_of_img = setTimeout(() => retrieve35Movies(url, genre, pageNumber), 200);
-        full_list_of_img.push(list_of_img)
-        pageNumber++;
-    }
-    console.log(full_list_of_img)
-    
-}
-
-
-// Fonction permettant de fetch 5 autres films après un clic sur une flèche sur le coté d'une catégorie
-let retrieve35Movies = function (url, genre='', pageNumber = 1) {
-    console.log("retieveOther7Movies is called");
-    divToChange = document.getElementById(div)
-        fetch(url + genre, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-                }
-            })
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                let movies_img = [];
-                for (let counter = 0; counter < 5; counter++) {
-                    let movie_components = []
-                    let movie_id = data.results[counter].id
-                    let img_url = data.results[counter].image_url
-                    movie_components = [movie_id, img_url]
-                    movies_img.push(movie_components)
-                }
-                return movies_img
-            })
-            .catch((err) => {
-                console.log("there is an error")
-                console.log(err)
-            })
-}
-
-
-
-
-
-// On ajoute des event Listener pour chaque flèche permettant de faire défiler les films
-BestRA.addEventListener("click", function() {
-        // il faut ici trouver un moyen de savoir sur quel page number on est
-        let new_movie_img = fetch_movies_onClick('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score', '', 1)
-        // Dans un 1er temps, déplier la liste pour mettre les 35 films côte à côte genre de 0-34
-        all_movie_img = []
-        for (list_of_5_movies in new_movie_img) {
-            for (movie in list_of_5_movies) {
-                all_movie_img.push(movie)
-            }
-        }
-        console.log(all_movie_img)
-        // suivant le page Number, on va prendre un bout de cette liste, qui va constituer les 7 images à afficher
-        // pour chacune des 7 images affichées actuellement, càd chaque div, enfant de la div principale dans laquelle on opère,
-        // on va changer deux attributs à l'image, son url et son id
-        // Donc boucle for qui compare chaque div avec chaque nouvelle image et qui change les propriétés
-
-})
-
-
-
 
